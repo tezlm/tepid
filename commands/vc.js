85be81431165e620a332ce4e8b8205daabc34d9a
BurningScarf347 <burning@scarf.347>
2021-08-13 12:14:15 -0700

    update discord.js

--- commands/vc.js
+++ commands/vc.js
@@ -4,6 +4,7 @@ const ytdl = require("ytdl-core");
 const fs = require("fs");
 const states = new Map();
 const audio = [];
+// a state is [vccontroller, audiocontroller, name, queue]
 
 for(let i of fs.readdirSync("assets")) {
 	audio.push([i, VC.createAudioResource("assets/" + i)]);
@@ -37,10 +38,44 @@ async function join(channel) {
 		guildId: channel.guild.id,
 		adapterCreator: channel.guild.voiceAdapterCreator,
 	});
-	states.set(channel.guild.id, [connection, null]);
+	connection.on(VC.VoiceConnectionStatus.Disconnected, async () => {
+		try {
+			await Promise.race([
+				entersState(connection, VoiceConnectionStatus.Signalling, 500),
+				entersState(connection, VoiceConnectionStatus.Connecting, 500),
+			]);
+		} catch (error) {
+			connection.destroy();
+		}
+	});
+	states.set(channel.guild.id, { conn: connection, queue: [] });
 }
 
-module.exports = async (msg, argv) => {
+async function next(id) {
+	const state = states.get(id);
+	const pop = state.queue.pop();
+	state.audio.stop();
+	if(!pop) return;
+	await load(id, await getSong(pop));
+	state.audio.once("idle", () => next(id));
+}
+
+async function load(id, song) {
+	const state = states.get(id);
+	state.name = song[0];
+	state.audio.play(song[1]);	
+}
+
+async function createAudio(state) {
+	state.audio = VC.createAudioPlayer();
+	state.audio.on("error", () => {
+		state.audio.stop();
+		state.audio = null;
+	});
+	state.conn.subscribe(state.audio);
+}
+
+module.exports = async (msg, argv, old) => {
 	const { channel } = msg.member.voice;
 	if(!channel) throw "you need to be in a channel";
 	if(channel.type !== "GUILD_VOICE") throw "not a voice channel";
@@ -53,39 +88,62 @@ module.exports = async (msg, argv) => {
 		case "play": {
 			if(!states.has(channel.guild.id)) await join(channel);
 			const state = states.get(channel.guild.id);
-			if(state[1]) {
-				if(state[1].state.status === "paused") {
-					state[1].unpause();
-					return "unpaused";
-				} else {
-					state[1].stop();
-				}
-			} else {
-				state[1] = VC.createAudioPlayer();
-				state[0].subscribe(state[1]);
+			if(state.audio && state.audio.state.status === "paused") {
+				state.audio.unpause();
+				return "unpaused";
+			} else if(!state.audio) {
+				createAudio(state);
 			}
-			const sent = await msg.reply("loading...");
+
+			const sent = old ? null : await msg.reply("loading...");
 			try {
-				const song = await getSong(argv.slice(2).join(" "));
-				state[1].play(song[1]);
-				if(!sent.deleted) await sent.edit(`playing ${song[0]}`);
+				if(old) {
+					if(state.lastMsgID !== msg.id) return "too late to change";
+					await state.audio.stop();
+					await old.edit("loading...");
+					await load(await getSong(argv.slice(2).join(" ")));
+					return `playing ${state.name}`;
+				} else {
+					state.lastMsgID = msg.id;
+					state.queue.push(argv.slice(2).join(" "));
+					if(state.audio.state.status === "idle") await next(channel.guild.id);
+					if(!sent.deleted) {
+						if(state.queue.length) {
+							await sent.edit(`queued your song`);
+						} else {
+							await sent.edit(`playing ${state.name}`);
+						}
+					}
+				}
 			} catch(err) {
+				if(old) return err;
 				if(!sent.deleted) await sent.edit(err);
 			}
 			return sent;
 		}
 		case "stop": {
 			if(!states.has(channel.guild.id)) throw "not in a voice channel";
-			states.get(channel.guild.id)[1].stop();
-			states.get(channel.guild.id)[0].destroy();
+			states.get(channel.guild.id).audio.stop();
+			states.get(channel.guild.id).conn.destroy();
 			states.delete(channel.guild.id);
 			return "stopping";
 		}
-		case "pause":
+		case "pause": {
 			if(!states.has(channel.guild.id)) throw "not in a voice channel";
-			const player = states.get(channel.guild.id)[1];
-			if(player.state.status === "paused") throw "already paused";
-			player.pause();
-			return "pausing";
+			const { audio } = states.get(channel.guild.id);
+			if(audio.state.status === "paused") throw "already paused";
+			audio.pause();
+			return "paused";
+		}
+		case "playing": {
+			if(!states.has(channel.guild.id)) throw "not in a voice channel";
+			return `playing: ${states.get(channel.guild.id).name}`;
+		}
+		case "skip": {
+			if(!states.has(channel.guild.id)) throw "not in a voice channel";
+			await next(channel.guild.id);
+			return `playing: ${states.get(channel.guild.id).name}`;
+		}
 	}
+	throw "unknown command";
 }
--- index.js
+++ index.js
@@ -21,21 +21,22 @@ client.on("ready", () => {
 });
 
 // parse argv
-async function parse(msg) {
+async function parse(msg, sent) {
 	if(msg.content[0] !== ':') return;
 	const argv = msg.content.slice(1).split(" ");
 	if(!commands.has(argv[0])) return;
 	try {
-		return await commands.get(argv[0])(msg, argv);
+		return await commands.get(argv[0])(msg, argv, sent);
 	} catch(err) {
 		return new Discord.MessageEmbed()
 			.setTitle(err)
 			.setColor(config.color.error);
 	}
 }
+
 // main bot
 client.on("messageCreate", async (msg) => {
-	const res = await parse(msg);
+	const res = await parse(msg, null);
 	if(!res) return;
 	if(res instanceof Discord.MessageEmbed) {
 		listening.set(msg.id, await msg.reply({ embeds: [res] }));
@@ -50,7 +51,7 @@ client.on("messageUpdate", async (old, msg) => {
 	if(!listening.has(msg.id)) return;
 	const sent = listening.get(msg.id);
 	if(sent.deleted) return;
-	const res = await parse(msg);
+	const res = await parse(msg, sent);
 	if(!res) return;
 	if(res instanceof Discord.MessageEmbed) {
 		await sent.edit({ embeds: [res] });
@@ -64,11 +65,5 @@ client.on("messageDelete", async (msg) => {
 	if(listening.get(msg.id).deleted) return;
 	await listening.get(msg.id).delete();
 });
-
-// client.on("messageDelete", async (msg) => {
-	// if(!listening.has(msg.id)) return;
-	// if(listening.get(msg.id).deleted) return;
-	// await listening.get(msg.id).delete();
-// });
 
 client.login(process.env.TOKEN);
