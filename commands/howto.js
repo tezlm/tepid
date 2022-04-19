0044a7d9c51b3fc02f965bdbff157c94c621dca2
tezlm <zestylemonade@zibifods.eu.org>
2022-04-18 19:47:25 -0700

    speed up howto

--- commands/howto.js
+++ commands/howto.js
@@ -1,19 +1,10 @@
-import bent from "bent";
+import google from "googlethis";
 import { MessageEmbed } from "discord.js";
 import config from "../config.js";
-const search = bent("json", "https://dynabyte.ca/search");
-
-async function fetch(url, retries = 3) {
-	if(retries < 0) throw "couldn't fetch";
-
-	const { results } = await search(url);
-	if(!results.length) return fetch(url, retries - 1);
-	return results;
-}
 
 export default async (msg, argv) => {
 	const query = argv.slice(1).join(" ").trim();
-	const results = await fetch(`?format=json&q=how%20to%20${encodeURIComponent(query)}`);
+	const { results } = await google.search("how to " + query);
 	const embed = new MessageEmbed();
 	let text = "";
 	for(let result of results.slice(0, 5)) {
